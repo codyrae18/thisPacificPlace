0fd286c9bc323d31aa1efa902438ad1c32a8da88
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-10-01 13:30:19 -0500

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   resources :favorites
   resources :cities
   resources :users
+  resources :sessions, only: [:create, :destroy]
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+luzon = Location.create(name: "Luzon")
+visayas = Location.create(name: "Visayas")
+mindanao = Location.create(name: "Mindanao")
+
+manila = City.create(name: "Manila", location_id: '1')
+baguio = City.create(name: "Baguio", location_id: '1')
+banaue = City.create(name: "Banaue", location_id: '1')
+legazpi = City.create(name: "Legazpi", location_id: '1')
+
+bohol = City.create(name: "Bohol", location_id: '2')
+puertoprincesa = City.create(name: "Puerto Princesa", location_id: '2')
+cebu = City.create(name: "Cebu", location_id: '2')
+boracay = City.create(name: "Boracay", location_id: '2')
+
+hinatuan = City.create(name: "Hinatuan", location_id: '3')
+davao = City.create(name: "Davao", location_id: '3')
+
