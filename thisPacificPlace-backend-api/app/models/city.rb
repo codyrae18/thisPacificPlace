242d81c571f973cac06a045ad7d54b65af118ddf
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-10-02 18:50:09 -0500

    front end development

--- thisPacificPlace-backend-api/app/models/city.rb
+++
@@ -1,6 +0,0 @@
-class City < ApplicationRecord
-    belongs_to :location
-    has_many :activities
-    has_many :foods
-    
-end
--- thisPacificPlace-frontEnd/src/index.js
+++ thisPacificPlace-frontEnd/src/index.js
@@ -2,75 +2,142 @@ const citiesURL = `http://localhost:3000/cities`
 const usersURL = `http://localhost:3000/users`
 const photosURL = `http://localhost:3000/photos`
 const foodsURL = `http://localhost:3000/foods`
+const activitiesURL = `http://localhost:3000/activities`
+const cityWeatherApi = `http://api.openweathermap.org/data/2.5/weather?q=Manila,ph`
+// const cityWithFood = "http://localhost:3000/cities/"
 
 const cityList = document.getElementById('list-group');
 const cityClass = document.getElementsByClassName('list-group-item');
-let cityDetail = document.getElementById('city-detail');
-let cityH2 = document.createElement('h2');
-let cityImg = document.createElement('img');
-let name = document.getElementById('name')
-let lowerDiv = document.getElementById('lower')
-let loginFormId = document.getElementById('login')
+const cityDetail = document.getElementById('city-detail');
+const cityH2 = document.createElement('h2');
+const cityImg = document.createElement('img');
+const name = document.getElementById('name')
+const lowerDiv = document.getElementById('lower')
+const loginFormId = document.getElementById('login')
+const foodName = document.createElement('h3')
+const foodsPhoto = document.createElement('img')
+const activityDetail = document.getElementById('activity-detail')
+const listOfActivity = document.getElementById("list-of-activity")
+const singleActivityDetail = document.getElementById('single-activity-detail')
+const activityPhoto = document.createElement('img')
+const activityDescription = document.createElement('p')
+
+
 document.addEventListener('DOMContentLoaded', event => {
-   
-  fetch(citiesURL)
-   .then(resp => resp.json())
-   .then(cityData => {
-       displaycities(cityData);
-       login(cityData);
-      //  console.log("City Data: ", cityData)
-   })
-
-
-   function displaycities(cityData) {
-       for(let oneCity of cityData) {
-            console.log("Just one", oneCity)
-           let li = document.createElement('li')
-           li.setAttribute('class', 'list-group-item')
-           li.innerText = oneCity.name;
-           cityList.appendChild(li)
-
-           li.addEventListener('click', event => {
-             console.log("event", event)
-             cityImg.src = oneCity.image_url;
-             cityH2.innerText = oneCity.name;
-             cityDetail.appendChild(cityImg)
-             cityDetail.appendChild(cityH2) 
-             displayFoodPhoto(cityData);                                                     
-           })
+//  weatherFetching()
+ fetch(citiesURL)
+  .then(resp => resp.json())
+  .then(cityData => {
+      displaycities(cityData);
+      login(cityData);
+     //  console.log("City Data: ", cityData)
+  })
+  function displaycities(cityData) {
+      for(let oneCity of cityData) {
+         // console.log("one city info", oneCity)
+          let li = document.createElement('li')
+          li.setAttribute('class', 'list-group-item')
+          li.innerText = oneCity.name;
+          cityList.appendChild(li)
+          li.addEventListener('click', event => {
+           //  console.log("my click", li.innerText)
+            activityDetail.innerText = ""
+            listOfActivity.innerText = ""
+            cityDetail.innerText = ""
+            cityImg.src = oneCity.image_url;
+            cityH2.innerText = oneCity.name;
+            cityDetail.appendChild(cityH2)
+            cityDetail.appendChild(cityImg)
+            displayFoodPhoto(oneCity);
+            displayActivities(oneCity);
+          })
+          
+        
+          
+      }
+  }
+  function login(cityData) {
+      loginFormId.addEventListener('submit', event => {
+        event.preventDefault();
+        let loginValue = name.value;
+        //  console.log("Login Value: ", loginValue)
+        let data = {
+          name: name.value
+         }
+         let object = {
+           method: 'POST',
+           headers: {
+             'Accept': 'application/json',
+             'Content-Type': 'application/json'
+           },
+           body: JSON.stringify(data)
+         }
+         fetch(usersURL, object)
+         .then(response => console.log("response after post: ", response.json()))
+       })
+  }
+  function displayFoodPhoto(oneCity) {
+   fetch(foodsURL)
+     .then(resp => resp.json())
+     .then(foodData => {
+       // console.log('FOOD DATA :', foodData)
+       let foodh2 = document.createElement('h2')
+       foodh2.innerText = "Main Dish"
+       cityDetail.appendChild(foodh2)
+       for (let singleFood of foodData) {
+         // console.log("Single Food", singleFood)
+         if (singleFood.city_id === oneCity.id) {
+           foodName.innerText = singleFood.name
+           foodsPhoto.src = singleFood.image_url
+           cityDetail.appendChild(foodName)
+           cityDetail.appendChild(foodsPhoto)
+         }
        }
-   }
-   function login(cityData) {
-       loginFormId.addEventListener('submit', event => {
-           event.preventDefault();
-           let loginValue = name.value;
-           console.log(loginValue)
-           let data = {
-               name: name.value
+     })
+  }
+
+  function displayActivities(oneCity) {
+    fetch(activitiesURL)
+    .then(resp => resp.json())
+      .then(activityData => {
+        // console.log('activity DATA :', activityData)
+        let activityh2 = document.createElement('h2')
+        activityh2.innerText = "ACTIVITIES"
+        activityDetail.appendChild(activityh2)
+        for (let singleActivity of activityData) {
+          // console.log("Single Activity", singleActivity)
+          if (singleActivity.city_id === oneCity.id) {
+            let activityName = document.createElement('li')
+            activityName.innerText = singleActivity.name
+            listOfActivity.appendChild(activityName)
+            activityDetail.appendChild(listOfActivity)
+            activityName.addEventListener('click', event => {
+                displayActivityPhoto(singleActivity)
+                singleActivityDetail.appendChild(activityPhoto)
+                singleActivityDetail.appendChild(activityDescription)
+                listOfActivity.appendChild(singleActivityDetail)
+                activityName.appendChild(singleActivityDetail)
+              })
              }
-             let object = {
-               method: 'POST',
-               headers: {
-                 'Accept': 'application/json',
-                 'Content-Type': 'application/json'
-               },
-               body: JSON.stringify(data)
-             }
-             fetch(usersURL, object)
-             .then(response => console.log("response after post: ", response.json()))
-           })
-
+          }
+      })
    }
-
-   function displayFoodPhoto(cityData) {
-
-    fetch(foodsURL)
-      .then(resp => resp.json())
-      .then(foodData => {
-        // console.log('FOOD DATA :', foodData)
-        for (let singleFood of foodData) {
-          // console.log("Single Food", singleFood)
+   function displayActivityPhoto(singleActivity) {
+    //  console.log("photo: ", singleActivity)
+    fetch(photosURL)
+    .then(resp => resp.json())
+    .then(photoData => {
+        for (let singlePhotoData of photoData) {
+          if (singlePhotoData.activity_id === singleActivity.id) {
+            activityDescription.innerText = singleActivity.description
+            activityPhoto.src = singlePhotoData.image_url
+          }
         }
       })
    }
-})
+   
+ })
+ 
+ 
+ 
+ 
